57a419c980448ba5620fe92261a44a1247d955d9
Eun Sik Jung <rinechran@outlook.jp>
2020-04-22 22:51:46 +0900

    feat : remover

---
+++ autoRemover/FileRemover.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoRemoveNet
+{
+    public class FileRemover
+    {
+        public FileRemover()
+        {
+            Format = "yyyy-MM-dd";
+            ExpireDay = -30;
+            CurrentTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+        }
+
+
+        public bool IsDateValieDate(string str)
+        {
+            try
+            {
+                DateTime.ParseExact(str, Format, System.Globalization.CultureInfo.CurrentCulture);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool IsExpire(string str)
+        {
+            DateTime dateMin = CurrentTime.AddDays(ExpireDay);
+            DateTime fileDate = DateTime.ParseExact(str, Format, System.Globalization.CultureInfo.CurrentCulture)
+                .AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            if (DateTime.Compare(fileDate,dateMin) > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void Run()
+        {
+            if (Path == "")
+            {
+                throw new ArgumentException("Not Setting Path");
+            }
+            string [] dirs = Directory.GetDirectories(Path);
+            foreach (string dir in dirs)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+                if (IsDateValieDate(dirInfo.Name) && IsExpire(dirInfo.Name))
+                {
+#if DEBUG
+                    Console.WriteLine("Delete folder " + dirInfo.Name);
+#endif
+                    dirInfo.Delete(true);
+                }
+                else
+                {
+#if DEBUG
+                    Console.WriteLine("Not Delete folder " + dirInfo.Name);
+#endif
+                }
+
+            }
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+        public string Format
+        {
+            get;
+            set;
+        }
+        public int ExpireDay
+        {
+            get;
+            set;
+        }
+        private DateTime CurrentTime
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ autoRemover/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoRemoveNet
+{
+
+    class Program
+    {
+        static void helpCommand()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("this program remove yyyy-MM-dd");
+            Console.WriteLine("-path folder path (example D:\\test)");
+            Console.WriteLine("-expire expireDay (default 30 day)");
+            Console.WriteLine("-format format (default yyyy-MM-dd)");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+
+        }
+        static void Main(string[] args)
+        {
+            FileRemover fileRemover = new FileRemover();
+
+            if (args.Length == 0)
+            {
+                helpCommand();
+            }
+            else if(args.Length%2==0)
+            {
+                int i = 0;
+                while (i < args.Length)
+                {
+                    string tag = args[i];
+                    string behaver = args[i + 1];
+
+                    switch (tag)
+                    {
+                        case "-path":
+                            fileRemover.Path = behaver;
+                            break;
+                        case "-format":
+                            fileRemover.Format = behaver;
+                            break;
+                        case "-expire":
+                            fileRemover.ExpireDay = -Convert.ToInt32(behaver);
+                            break;
+
+                    }
+                    i += 2;
+
+                }
+                fileRemover.Run();
+
+            }
+        }
+    }
+}
